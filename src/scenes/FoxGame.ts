c83433f161deb9d6fa74323e8b71929e7f8d82cb
kevinkons <kevin.kons@hotmail.com>
2023-12-21 17:26:25 -0300

    new map

--- src/scenes/FoxGame.ts
+++ src/scenes/FoxGame.ts
@@ -70,12 +70,12 @@ export class FoxGame extends Phaser.Scene {
     })
 
 
-    const debugGraphics = this.add.graphics().setAlpha(0.75);
-    this.treesLayer?.renderDebug(debugGraphics, {
-      tileColor: null,
-      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
-      faceColor: new Phaser.Display.Color(40, 39, 37, 255)
-    });
+    // const debugGraphics = this.add.graphics().setAlpha(0.75);
+    // this.treesLayer?.renderDebug(debugGraphics, {
+    //   tileColor: null,
+    //   collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+    //   faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+    // });
 
 
     this.character = new Character(this, 850, 740, "character");
