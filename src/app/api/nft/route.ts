ced8d12cc35dc35459061bae06b695ca77cd6d8c
kevinkons <kevin.kons@hotmail.com>
2024-01-17 14:39:26 -0300

    send nft implementation

--- src/app/api/nft/route.ts
+++ src/app/api/nft/route.ts
@@ -1,36 +1,39 @@
 import { Errors } from "@/utils/errors";
+import { ethers } from "ethers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const { "g-recaptcha-response": captchaResponse, level } = await request.json()
+  const { "g-recaptcha-response": captchaResponse, level, userAddress } = await request.json()
 
+  if (!isCaptchaValid(captchaResponse)) {
+    return new NextResponse(JSON.stringify({ message: Errors.CAPTCHA_FAILED }), { status: 404 });
+  }
+
+  const txHash = sendNft(userAddress, level)
+
+  return NextResponse.json({ txHash });
+}
+
+const isCaptchaValid = async (captchaResponse: string): Promise<boolean> => {
   const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captchaResponse}`,
-
   });
 
   const responseData = await response.json();
+  return responseData.success
+}
 
-  return new NextResponse(JSON.stringify({ message: Errors.CAPTCHA_FAILED }), { status: 404 });
+const sendNft = async (to: string, level: number): Promise<string> => {
+  const mintFunctionInterface = new ethers.utils.Interface(["function mint(address,uint256,uint256,bytes)"]);
 
-  if (responseData.success) {
-    // response.send('reCAPTCHA verification successful!');
-  } else {
-    return new NextResponse(null, { status: 404 });
-  }
+  const provider = new ethers.providers.JsonRpcProvider(process.env.LINEA_RPC_URL)
+  const wallet = new ethers.Wallet(process.env.CONTRACT_OWNER_PRIVATE_KEY!, provider);
+  const skinsContract = new ethers.Contract(process.env.LIL_FOX_SKINS_CONTRACT_ADDRESS!, mintFunctionInterface, wallet);
 
-  // if (response.data.success) {
-  //   // The reCAPTCHA was successful, proceed with your logic
-  //   res.send('reCAPTCHA verification successful!');
-  // } else {
-  //   // The reCAPTCHA failed, handle accordingly
-  //   res.status(400).send('reCAPTCHA verification failed');
-  // }
-
-
-  return NextResponse.json({ message: "Hello, world!" });
+  const tx = await skinsContract.mint(to, level, 1, "0x");
+  return tx.hash
 }
