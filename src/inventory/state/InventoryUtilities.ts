d6ccb5f269e01361936be816229ce5fb934e6008
kevinkons <kevin.kons@hotmail.com>
2024-02-29 20:29:40 -0300

    Adding logic to moveItemToSlot

--- src/inventory/state/InventoryUtilities.ts
+++ src/inventory/state/InventoryUtilities.ts
@@ -18,21 +18,26 @@ export const addToInventory = (actor: GameEntity, item: GameEntity) => {
   inventoryEvents.emit(InventoryEvent.ITEM_ADDED, item);
 }
 
+
 export function moveItemToSlot(itemId: string, targetSlotIndex?: number) {
   const itemEntity = world.entityManager.getEntityByName(itemId) as GameEntity;
+
   const currentSlot = playerEntity.inventory_mutable.slots.find(
     (i) => i.item === itemEntity.entityId.value
   );
 
   const removedItemId = currentSlot!.removeItem();
 
-  const targetSlot = playerEntity.inventory_mutable.firstAvailableSlot()
+  const targetSlot =
+    targetSlotIndex !== undefined
+      ? playerEntity.inventory_mutable.slots[targetSlotIndex]
+      : playerEntity.inventory_mutable.firstAvailableSlot();
 
-  targetSlot?.addItem(removedItemId)
+  targetSlot?.addItem(removedItemId);
   itemEntity.pickedUp_mutable.slotIndex = targetSlot!.slotIndex;
 
   inventoryEvents.emit(InventoryEvent.ITEM_MOVED, {
     currentSlotIndex: currentSlot!.slotIndex,
     item: itemEntity,
-  })
+  });
 }
